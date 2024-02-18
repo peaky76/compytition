bb627dc477a36b03450e7f4d1fad8818a5caad0f
peaky76 <robertjamespeacock@gmail.com>
2024-02-18 20:10:27 +0000

    test: Rank comparison

--- tests/test_rank.py
+++ tests/test_rank.py
@@ -16,9 +16,12 @@ def test_rank_can_be_initialised_with_tied_keyword():
     assert Rank(1, tied=True)
 
 def test_rank_cannot_be_initialised_with_float():
-    with pytest.raises(ValueError):  # noqa: PT011
+    with pytest.raises(ValueError):  # noqa: PT011 until upgrade to peak_utility 0.6.1
         Rank(1.0)
 
 def test_rank_cannot_be_initialised_with_invalid_string():
-    with pytest.raises(ValueError):  # noqa: PT011
+    with pytest.raises(ValueError):  # noqa: PT011 until upgrade to peak_utility 0.6.1
         Rank('a')
+
+def test_rank_can_be_compared():
+    assert Rank(1) > Rank(2)
