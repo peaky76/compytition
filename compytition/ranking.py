74fd60183286e31088cdda4f3ad7dfe576df30b7
peaky76 <robertjamespeacock@gmail.com>
2024-02-18 20:55:35 +0000

    fix: Ranking enforces consecutive, tie-considerate Ranks

--- compytition/ranking.py
+++ compytition/ranking.py
@@ -6,5 +6,18 @@ from .rank import Rank
 class Ranking(dict):
     def __init__(self, ranking: dict[Any, int | str | Rank]) -> None:        
         ranking = {k: Rank(v) for k, v in ranking.items()}
+
+        previous_rank = 0
+        tied = 0
+        for rank in sorted(ranking.values(), reverse=True):
+            current_rank = rank
+            if int(current_rank) == int(previous_rank):
+                tied += 1
+            else:
+                if int(previous_rank) + 1 + tied != int(current_rank):
+                    message = "Ranks must be consecutive" if tied == 0 else "Ranks must account for ties"
+                    raise ValueError(message)
+                tied = 0
+            previous_rank = current_rank
         
         super().__init__(ranking)
--- tests/test_ranking.py
+++ tests/test_ranking.py
@@ -15,3 +15,15 @@ def test_ranking_can_be_initialised_with_dict_of_objects_and_ranks():
 def test_ranking_cannot_be_initialised_with_invalid_dict():
     with pytest.raises(ValueError):  # noqa: PT011 until upgrade to peak_utility 0.6.1
         Ranking({'a': "winner", 'b': 2.0, 'c': 3})
+
+def test_ranking_cannot_be_initialised_with_non_consecutive_ranks():
+    with pytest.raises(ValueError): # noqa: PT011 until upgrade to peak_utility 0.6.1
+        Ranking({'a': 1, 'b': 3, 'c': 4})
+
+def test_ranking_cannot_be_initialised_with_consecutive_int_ranks_after_tied():
+    with pytest.raises(ValueError):  # noqa: PT011 until upgrade to peak_utility 0.6.1
+        Ranking({'a': 1, 'b': 2, 'c': 2, 'd': 3})
+
+def test_ranking_cannot_be_initialised_with_consecutive_obj_ranks_after_tied():
+    with pytest.raises(ValueError):  # noqa: PT011 until upgrade to peak_utility 0.6.1
+        Ranking({'a': Rank(1), 'b': Rank(2), 'c': Rank(2), 'd': Rank(3)})
