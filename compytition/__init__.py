5031a6a27cc74fb115071f2d79b042ad39eeca5a
peaky76 <robertjamespeacock@gmail.com>
2024-02-18 20:36:27 +0000

    feat: Ranking class

--- compytition/__init__.py
+++ compytition/__init__.py
@@ -1,3 +1,4 @@
 from .rank import Rank
+from .ranking import Ranking
 
-__all__ = ['Rank']
+__all__ = ['Rank', 'Ranking']
---
+++ tests/test_ranking.py
@@ -0,0 +1,17 @@
+import pytest
+
+from compytition import Rank, Ranking
+
+
+def test_ranking_can_be_initialised_with_dict_of_objects_and_ints():
+    assert Ranking({'a': 1, 'b': 2, 'c': 3})
+
+def test_ranking_can_be_initialised_with_dict_of_objects_and_strs():
+    assert Ranking({'a': '1', 'b': '2', 'c': '3=', 'd': '3='})
+
+def test_ranking_can_be_initialised_with_dict_of_objects_and_ranks():
+    assert Ranking({'a': Rank(1), 'b': Rank(2), 'c': Rank(3, tied=True), 'd': Rank(3, tied=True)})
+
+def test_ranking_cannot_be_initialised_with_invalid_dict():
+    with pytest.raises(ValueError):  # noqa: PT011 until upgrade to peak_utility 0.6.1
+        Ranking({'a': "winner", 'b': 2.0, 'c': 3})
