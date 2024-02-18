7fecae059a7ff63ea7c97d763316c6163e4faa7c
peaky76 <robertjamespeacock@gmail.com>
2024-02-18 20:45:05 +0000

    refactor: rely on Rank ValueError in Ranking.__init__

--- compytition/ranking.py
+++ compytition/ranking.py
@@ -4,9 +4,7 @@ from .rank import Rank
 
 
 class Ranking(dict):
-    def __init__(self, ranking: dict[Any, int | str | Rank]) -> None:
-        for k, v in ranking.items():
-            if not isinstance(v, (int, str, Rank)):
-                raise ValueError(f"Invalid value for {k}: {v}")
-
-        super().__init__({k: Rank(v) for k, v in ranking.items()})
+    def __init__(self, ranking: dict[Any, int | str | Rank]) -> None:        
+        ranking = {k: Rank(v) for k, v in ranking.items()}
+        
+        super().__init__(ranking)
