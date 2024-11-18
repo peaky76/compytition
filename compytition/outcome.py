7027e29f929201c829685bb67fb21c26c21118d7
peaky76 <robertjamespeacock@gmail.com>
2024-11-18 22:31:39 +0000

    docs: Outcome docstrings

--- compytition/outcome.py
+++ compytition/outcome.py
@@ -2,6 +2,18 @@ from enum import Enum
 
 
 class Outcome(Enum):
+    """
+    Represents the outcome of an event.
+
+    Attributes
+    ----------
+    WIN : Outcome
+        Represents a win outcome.
+    LOSE : Outcome
+        Represents a lose outcome.
+    DRAW : Outcome
+        Represents a draw outcome.
+    """
     WIN = 1
     LOSE = 2
     DRAW = 3
