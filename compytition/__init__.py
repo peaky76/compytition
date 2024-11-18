222f49dcf939586cf16224c199a3844f4afcf91c
peaky76 <robertjamespeacock@gmail.com>
2024-11-18 09:13:12 +0000

    feat: Outcome enum

--- compytition/__init__.py
+++ compytition/__init__.py
@@ -1,5 +1,6 @@
+from .outcome import Outcome
 from .rank import Rank
 from .rank_list import RankList
 from .ranking import Ranking
 
-__all__ = ['Rank', 'RankList', 'Ranking']
+__all__ = ['Outcome', 'Rank', 'RankList', 'Ranking']
---
+++ compytition/outcome.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Outcome(Enum):
+    WIN = 1
+    LOSE = 2
+    DRAW = 3
+    
